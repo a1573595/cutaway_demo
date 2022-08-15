202833be1b029bd0b8151cac8798393d1c9147ae
a1573595 <a1573595@gmail.com>
2022-08-15 10:06:09 +0800

    Update ContextGoRouterHelper & GoRouterHelper

--- lib/tool/extensions.dart
+++ lib/tool/extensions.dart
@@ -1,69 +1,69 @@
-// import 'package:cutaway/router/route_utils.dart';
-// import 'package:cutaway/tool/shared_prefs.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:go_router/go_router.dart';
-//
-// // TODO('更換APP寫法')
-// extension ContextGoRouterHelper on BuildContext {
-//   void goWithLogin(String location, {Object? extra}) {
-//     var isLogin = sharedPrefs.getIsLogin();
-//
-//     if (isLogin) {
-//       go(location, extra: extra);
-//     } else {
-//       push(AppPage.welcome.fullPath);
-//     }
-//   }
-//
-//   void pushWithLogin(String location, {Object? extra}) {
-//     var isLogin = sharedPrefs.getIsLogin();
-//
-//     if (isLogin) {
-//       push(location, extra: extra);
-//     } else {
-//       push(AppPage.welcome.fullPath);
-//     }
-//   }
-//
-//   void replaceWithLogin(String location, {Object? extra}) {
-//     var isLogin = sharedPrefs.getIsLogin();
-//
-//     if (isLogin) {
-//       replace(location, extra: extra);
-//     } else {
-//       push(AppPage.welcome.fullPath);
-//     }
-//   }
-// }
-//
-// extension GoRouterHelper on GoRouter {
-//   void goWithLogin(String location, {Object? extra}) {
-//     var isLogin = sharedPrefs.getIsLogin();
-//
-//     if (isLogin) {
-//       go(location, extra: extra);
-//     } else {
-//       push(AppPage.welcome.fullPath);
-//     }
-//   }
-//
-//   void pushWithLogin(String location, {Object? extra}) {
-//     var isLogin = sharedPrefs.getIsLogin();
-//
-//     if (isLogin) {
-//       push(location, extra: extra);
-//     } else {
-//       push(AppPage.welcome.fullPath);
-//     }
-//   }
-//
-//   void replaceWithLogin(String location, {Object? extra}) {
-//     var isLogin = sharedPrefs.getIsLogin();
-//
-//     if (isLogin) {
-//       replace(location, extra: extra);
-//     } else {
-//       push(AppPage.welcome.fullPath);
-//     }
-//   }
-// }
+import 'package:cutaway/router/route_utils.dart';
+import 'package:cutaway/tool/shared_prefs.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+
+// TODO('更換APP寫法')
+extension ContextGoRouterHelper on BuildContext {
+  void goWithLogin(String location, {Object? extra}) async {
+    var isLogin = await sharedPrefs.getIsLogin();
+
+    if (isLogin) {
+      go(location, extra: extra);
+    } else {
+      push(AppPage.welcome.fullPath);
+    }
+  }
+
+  void pushWithLogin(String location, {Object? extra}) async {
+    var isLogin = await sharedPrefs.getIsLogin();
+
+    if (isLogin) {
+      push(location, extra: extra);
+    } else {
+      push(AppPage.welcome.fullPath);
+    }
+  }
+
+  void replaceWithLogin(String location, {Object? extra}) async {
+    var isLogin = await sharedPrefs.getIsLogin();
+
+    if (isLogin) {
+      replace(location, extra: extra);
+    } else {
+      push(AppPage.welcome.fullPath);
+    }
+  }
+}
+
+extension GoRouterHelper on GoRouter {
+  void goWithLogin(String location, {Object? extra}) async {
+    var isLogin = await sharedPrefs.getIsLogin();
+
+    if (isLogin) {
+      go(location, extra: extra);
+    } else {
+      push(AppPage.welcome.fullPath);
+    }
+  }
+
+  void pushWithLogin(String location, {Object? extra}) async {
+    var isLogin = await sharedPrefs.getIsLogin();
+
+    if (isLogin) {
+      push(location, extra: extra);
+    } else {
+      push(AppPage.welcome.fullPath);
+    }
+  }
+
+  void replaceWithLogin(String location, {Object? extra}) async {
+    var isLogin = await sharedPrefs.getIsLogin();
+
+    if (isLogin) {
+      replace(location, extra: extra);
+    } else {
+      push(AppPage.welcome.fullPath);
+    }
+  }
+}
